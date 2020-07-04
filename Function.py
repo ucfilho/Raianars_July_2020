100561be57fc18bb7b69708b30a0c42f8006eaae
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-07-03 21:14:31 -0300

    Update Function.py

--- Function.py
+++ Function.py
@@ -170,12 +170,22 @@ def Rosenbrock(x):
         fun =fun+ 100*(x[i+1]-x[i]**2)**2 + (1-x[i])**2
     return fun 
 # Global Minimum: 0 , domain=[-30,30]
-
+'''
 def HappyCat(x): 
     alpha=1. / 8
     s = sum(x**2) 
     return ((s - len(x))**2)**alpha + (s / 2 + sum(x)) / len(x) + 0.5 
 # global mimima at (-1,-1,...,-1) - f(x)=0  - d=[-2,2] (qlq dominio)
+'''
+
+def HappyCat(x): 
+    alpha=1. / 8
+    s=0
+    for i in x:
+	s=s+x*x
+    return ((s - len(x))**2)**alpha + (s / 2 + sum(x)) / len(x) + 0.5 
+# global mimima at (-1,-1,...,-1) - f(x)=0  - d=[-2,2] (qlq dominio)
+
 
 def Alpinen2(x):
   prod=1
